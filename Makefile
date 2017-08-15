c9a1827e30f6856ff4477e1b074120a7ee2b69fe
Kevin Pham <phamkevinx@gmail.com>
2017-08-15 18:13:58 +0000

    create basic framework to generate a random maze
    Add a prompt to ask the user to specify how big they want the maze to be.
    Need to add sanity checks. Also show how the rooms are numbered and print
    out the generated maze, which at this point is a simple chess board.

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	gcc maze_maker.cpp -lstdc++ -o maze_maker
+clean:
+	rm maze_maker
---
+++ maze_maker.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+	int size;
+	
+	std::cout << "Input the desired side length for the maze: ";
+	std::cin >> size; 	
+
+	// create 2D array to be our maze	
+	int** maze = new int*[size];
+	for(int i = 0; i < size; i++){
+		maze[i] = new int[size];
+	}
+	
+	// nested for loops to label each room starting from 0
+	std::cout << "Rooms by number:\n";	
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			maze[i][j] = (i * size) + j;
+			std::cout << maze[i][j] << "\t"; 
+		}
+		std::cout << "\n\n";
+	}
+	
+	/*Print out the current maze, which at this point would look
+	* like a chess board
+	*/
+	std::cout << "Current Maze:\n";
+	for(int i = 0; i <= size; i++){
+		for(int j = 0; j < size; j++){
+			if(i == 0){
+				std::cout << " _";
+			} else {
+				std::cout << "|_";
+			}
+			if(j == (size - 1) && i != 0){
+				std::cout << "|";
+			}
+		}	
+		std::cout << std::endl;
+	}
+	return 0;
+} 
